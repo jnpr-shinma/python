4d8baf6df01be60c301f7c37ddef31d74aac996a
Shin Ma <shinma@juniper.net>
2016-01-20 11:01:10 +0800

    d

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(
-        name = "demo",
+        name = "demo1",
         version="0.1.0",
         packages = find_packages(),
         zip_safe = False,
