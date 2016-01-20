7297ab8cb51765b72d0bc7da729c9e091bd09f17
Shin Ma <shinma@juniper.net>
2016-01-20 11:09:44 +0800

    d

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@
 from setuptools import setup, find_packages
 
 setup(
-        name = "demo1",
+        name = "demo",
         version="0.1.0",
         packages = find_packages(),
         zip_safe = False,
 
         description = "egg test demo.",
         long_description = "egg test demo, haha.",
-        author = "amoblin",
-        author_email = "amoblin@ossxp.com",
+        author = "maxin",
+        author_email = "maxin@maxin.com",
 
         license = "GPL",
         keywords = ("test", "egg"),
