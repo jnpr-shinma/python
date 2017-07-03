679d9c77476d0d6bcabc02e70e887a9137af5d71
Shin Ma <shinma@juniper.net>
2017-07-03 10:12:21 +0800

    d

--- urllib/test.py
+++ urllib/test.py
@@ -1,7 +1,10 @@
-import requests
-url="https://10.213.19.131:81/data-view-central/connections"
-token="980bbca62a51460d8501125d6ac30700"
-headers = {"X-AUTH-TOKEN": token}
-
-response = requests.get(url, headers=headers, verify=False)
-print response
+# import requests
+# url="https://10.213.19.131:81/data-view-central/connections"
+# token="980bbca62a51460d8501125d6ac30700"
+# headers = {"X-AUTH-TOKEN": token}
+#
+# response = requests.get(url, headers=headers, verify=False)
+# print response
+#
+d={}
+print len(d.get("d"))
